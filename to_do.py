a51bafa30b7e98189f295c02f827a6370573d6f0
Deepakdravid <156296402+Deepakdravid@users.noreply.github.com>
2024-02-06 21:26:55 +0530

    Update to_do.py

--- to_do.py
+++ to_do.py
@@ -9,22 +9,40 @@ while True:
     
     match user_action:
         case 'add':
-            todo=input("ENTER NEW TASK : ")
-            todos.append(todo)
-            file=open('todos.txt','w')
-            file.writelines(todos)
-            file.close()
+            n=int(input("ENTER THE NUMBER OF TASK TO ADD : "))
+          
+            for items in range(n):
+                todo=input("ENTER THE TASK : ")
+                found=False
+                for strings in todos:
+                    if strings.lower() in todo.lower() or todo.lower() in strings.lower():
+                        found=True
+                        break
+                if found:
+                    print("ALREADY ADDED")
+                else:
+                    todos.append(todo)
+            
         case 'show':
             for item in todos:
                 print(item)
             print("THESE ARE PENDING TASKS : ")
         
         case 'complete':
-            if(user_action=='complete'):
-                com=input("ENTER TASK TO COMPLETE : ")
-                todos.remove(com)
-                print("COMPLETED")
+            comp = input("ENTER TASK TO COMPLETE : ")
+            found = False
+            for task in todos:
+               if task.strip().lower() == comp.strip().lower():
+                  todos.remove(task)
+                  found = True
+                  print("COMPLETED")
+                  break
+            if not found:
+                print("Task not found in the list.")
+
             
                        
         case 'exit':
             break
+        
+
